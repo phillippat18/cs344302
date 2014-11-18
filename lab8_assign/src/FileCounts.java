772f4506453504f831947b9d2a2fd22441e85cc3
Kyle Haley <kylephaley@gmail.com>
2014-11-18 17:11:00 -0600

    Added try/catch, refactored accordingly

---
+++ lab8_assign/src/FileCounts.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class FileCounts {
+    private File file;
+    
+    public FileCounts(File file) {
+        this.file = file;
+    }
+ 
+    public int lineCount() {
+        return -1;
+    }
+    
+    public int tokenCount() {
+        return -1;
+    }
+    
+    public int charCount() {
+        return -1;
+    }
+    
+    public int byteCount() {
+        return -1;
+    }
+}
